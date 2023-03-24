44177b58b18c423dc03b8693a24241c1d7bf5cf2
sherin <skaryil@equinix.com>
2023-03-24 17:41:21 +0530

    added inserting to table

--- assignment-3/postgres.py
+++ assignment-3/postgres.py
@@ -2,12 +2,13 @@ import psycopg2
 
 class PostgresDB():
 
-
+    # connect to the DB
     def __init__(self) -> None:
 
         self.conn = psycopg2.connect(database="postgres") #, user="your_username", password="your_password", host="your_host")# port="your_port")
         self.cur = self.conn.cursor()
 
+    # create a table 
     def create_table(self, table_name, columns):
 
         # define the SQL query to create the table
@@ -20,33 +21,21 @@ class PostgresDB():
         self.conn.commit()
 
 
-    # def create_table(self,table_name, query):
-        
-    #     self.cur.execute(f"SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = '{table_name}')")
-    #     table_exists = self.cur.fetchone()[0]
-
-    #     if not table_exists:
-    #         # # create the departments table
-    #         # self.cur.execute(f"""
-    #         # CREATE TABLE {table_name} (
-    #         #     id SERIAL PRIMARY KEY,
-    #         #     name VARCHAR(50) NOT NULL
-    #         # )
-    #         # """)
-    #         self.cur.execute(query)
-    #         self.conn.commit()
-
+    # insert multiple rows into a table
     def insert_table(self, table_name, values):
 
-        sql = f"INSERT INTO TABLE {table_name} VALUES ({', '.join([val for val in values])})"
+        for vals in values:
 
-        # execute the SQL query to create the table
-        self.cur.execute(sql)
+            sql = f"INSERT INTO {table_name} VALUES ({', '.join(['%s'] * len(vals))})"
+            print(sql)
 
-        # commit the changes
-        self.conn.commit()
+            self.cur.execute(sql, tuple(vals))
+
+            # commit the changes
+            self.conn.commit()
 
 
+    # drop a table if it exists
     def drop_table(self, table_name):
 
         self.cur.execute(f"SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = '{table_name}')")
@@ -59,16 +48,17 @@ class PostgresDB():
             print("Does not exist")
 
 
-
+# Instantiating the Class object
 postgres = PostgresDB()
 
+# DROP the tables before execution
 postgres.drop_table('employees')
 postgres.drop_table('departments')
 
 
+# CREATE the required tables
 dept_columns = [('id','SERIAL PRIMARY KEY'),
                ('name','VARCHAR(50) NOT NULL')]
-
 postgres.create_table('departments', dept_columns)
 
 
@@ -77,66 +67,17 @@ emp_columns = [('id', 'SERIAL PRIMARY KEY'),
     ('last_name', 'VARCHAR(50) NOT NULL'),
     ('email', 'VARCHAR(50) NOT NULL UNIQUE'),
     ('department_id', 'INTEGER NOT NULL REFERENCES departments(id)')]
-
 postgres.create_table('employees', emp_columns)
 
-# # create the employees table
-# query = f"""
-# CREATE TABLE {table_name} (
-#     id SERIAL PRIMARY KEY,
-#     first_name VARCHAR(50) NOT NULL,
-#     last_name VARCHAR(50) NOT NULL,
-#     email VARCHAR(50) NOT NULL UNIQUE,
-#     department_id INTEGER NOT NULL REFERENCES departments(id)
-# )
-# """
 
-# values = [(100,"Computer"),
-#           (300,"BioTech"),
-#           (200,"Commerce")]
-
-# # define the SQL query to insert values into the table
-# sql = """
-# INSERT INTO departments (id, name)
-# VALUES (%s, %s)
-# """
-
-# define the values to insert into the table
+# INSERT values to the created tables
 values = [(100,"Computer"),
           (300,"BioTech"),
           (200,"Commerce")]
+postgres.insert_table('departments', values)
 
-# define the values to insert into the table
+
 values = [(1,"John", "Doe", "john.doe@example.com", 100),
           (2,"Jane", "Smith", "jane.smith@example.com", 200),
           (3,"Bob", "Johnson", "bob.johnson@example.com", 100)]
-
-# # execute the SQL query with the values
-# cur.executemany(sql, values)
-
-# # commit the changes
-# conn.commit()
-
-
-# # define the SQL query to insert values into the table
-# sql = """
-# INSERT INTO employees (id, first_name, last_name, email, department_id)
-# VALUES (%s, %s, %s, %s, %s)
-# """
-
-# # define the values to insert into the table
-# values = [(1,"John", "Doe", "john.doe@example.com", 100),
-#           (2,"Jane", "Smith", "jane.smith@example.com", 200),
-#           (3,"Bob", "Johnson", "bob.johnson@example.com", 100)]
-
-# # execute the SQL query with the values
-# cur.executemany(sql, values)
-
-# # commit the changes
-# conn.commit()
-
-
-
-# # close the cursor and the database connection
-# cur.close()
-# conn.close()
+postgres.insert_table('employees', values)
