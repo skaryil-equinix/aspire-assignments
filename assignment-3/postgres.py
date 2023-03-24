8ffab2361d81417a7d6f38c259b8d7cfac28ff00
sherin <skaryil@equinix.com>
2023-03-24 17:20:12 +0530

    drop table added

--- assignment-3/postgres.py
+++ assignment-3/postgres.py
@@ -1,100 +1,144 @@
 import psycopg2
 
-# connect to the PostgreSQL database
-conn = psycopg2.connect(database="postgres") #, user="your_username", password="your_password", host="your_host")# port="your_port")
-cur = conn.cursor()
+class PostgresDB():
 
 
-table_name = "employees"
-cur.execute(f"SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = '{table_name}')")
-table_exists = cur.fetchone()[0]
+    def __init__(self) -> None:
 
-# if the table exists, drop it
-if table_exists:
-    cur.execute(f"DROP TABLE {table_name}")
-    # commit the changes
-    conn.commit()
+        self.conn = psycopg2.connect(database="postgres") #, user="your_username", password="your_password", host="your_host")# port="your_port")
+        self.cur = self.conn.cursor()
 
-table_name = "departments"
-cur.execute(f"SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = '{table_name}')")
-table_exists = cur.fetchone()[0]
+    def create_table(self, table_name, columns):
 
+        # define the SQL query to create the table
+        sql = f"CREATE TABLE {table_name} ({', '.join([f'{column[0]} {column[1]}' for column in columns])})"
 
-# if the table exists, drop it
-if table_exists:
-    cur.execute(f"DROP TABLE {table_name}")
-    # commit the changes
-    conn.commit()
+        # execute the SQL query to create the table
+        self.cur.execute(sql)
+
+        # commit the changes
+        self.conn.commit()
 
 
+    # def create_table(self,table_name, query):
+        
+    #     self.cur.execute(f"SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = '{table_name}')")
+    #     table_exists = self.cur.fetchone()[0]
 
-table_name = "departments"
+    #     if not table_exists:
+    #         # # create the departments table
+    #         # self.cur.execute(f"""
+    #         # CREATE TABLE {table_name} (
+    #         #     id SERIAL PRIMARY KEY,
+    #         #     name VARCHAR(50) NOT NULL
+    #         # )
+    #         # """)
+    #         self.cur.execute(query)
+    #         self.conn.commit()
 
-# create the departments table
-cur.execute(f"""
-CREATE TABLE {table_name} (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
+    def insert_table(self, table_name, values):
 
-)
-""")
+        sql = f"INSERT INTO TABLE {table_name} VALUES ({', '.join([val for val in values])})"
 
-# commit the changes
-conn.commit()
+        # execute the SQL query to create the table
+        self.cur.execute(sql)
 
-table_name = "employees"
+        # commit the changes
+        self.conn.commit()
 
-# create the employees table
-cur.execute(f"""
-CREATE TABLE {table_name} (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL UNIQUE,
-    department_id INTEGER NOT NULL REFERENCES departments(id)
-)
-""")
 
-# commit the changes
-conn.commit()
+    def drop_table(self, table_name):
 
-# define the SQL query to insert values into the table
-sql = """
-INSERT INTO departments (id, name)
-VALUES (%s, %s)
-"""
+        self.cur.execute(f"SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = '{table_name}')")
+        table_exists = self.cur.fetchone()[0]
+
+        if  table_exists:
+
+            self.cur.execute(f"DROP TABLE {table_name}")
+            self.conn.commit()
+
+
+postgres = PostgresDB()
+
+postgres.drop_table('departments')
+
+
+
+
+dept_columns = [('id','SERIAL PRIMARY KEY'),
+               ('name','VARCHAR(50) NOT NULL')]
+
+# # create the departments table
+# query =  f"""
+#     CREATE TABLE {table_name} (
+#         id SERIAL PRIMARY KEY,
+#         name VARCHAR(50) NOT NULL
+#     )
+#     """
+
+emp_columns = [('id', 'SERIAL PRIMARY KEY'),
+    ('first_name', 'VARCHAR(50) NOT NULL'),
+    ('last_name', 'VARCHAR(50) NOT NULL'),
+    ('email', 'VARCHAR(50) NOT NULL UNIQUE'),
+    ('department_id', 'INTEGER NOT NULL REFERENCES departments(id)')]
+
+# # create the employees table
+# query = f"""
+# CREATE TABLE {table_name} (
+#     id SERIAL PRIMARY KEY,
+#     first_name VARCHAR(50) NOT NULL,
+#     last_name VARCHAR(50) NOT NULL,
+#     email VARCHAR(50) NOT NULL UNIQUE,
+#     department_id INTEGER NOT NULL REFERENCES departments(id)
+# )
+# """
+
+# values = [(100,"Computer"),
+#           (300,"BioTech"),
+#           (200,"Commerce")]
+
+# # define the SQL query to insert values into the table
+# sql = """
+# INSERT INTO departments (id, name)
+# VALUES (%s, %s)
+# """
 
 # define the values to insert into the table
 values = [(100,"Computer"),
           (300,"BioTech"),
           (200,"Commerce")]
 
-# execute the SQL query with the values
-cur.executemany(sql, values)
-
-# commit the changes
-conn.commit()
-
-
-# define the SQL query to insert values into the table
-sql = """
-INSERT INTO employees (id, first_name, last_name, email, department_id)
-VALUES (%s, %s, %s, %s, %s)
-"""
-
 # define the values to insert into the table
 values = [(1,"John", "Doe", "john.doe@example.com", 100),
           (2,"Jane", "Smith", "jane.smith@example.com", 200),
           (3,"Bob", "Johnson", "bob.johnson@example.com", 100)]
 
-# execute the SQL query with the values
-cur.executemany(sql, values)
+# # execute the SQL query with the values
+# cur.executemany(sql, values)
 
-# commit the changes
-conn.commit()
+# # commit the changes
+# conn.commit()
+
+
+# # define the SQL query to insert values into the table
+# sql = """
+# INSERT INTO employees (id, first_name, last_name, email, department_id)
+# VALUES (%s, %s, %s, %s, %s)
+# """
+
+# # define the values to insert into the table
+# values = [(1,"John", "Doe", "john.doe@example.com", 100),
+#           (2,"Jane", "Smith", "jane.smith@example.com", 200),
+#           (3,"Bob", "Johnson", "bob.johnson@example.com", 100)]
+
+# # execute the SQL query with the values
+# cur.executemany(sql, values)
+
+# # commit the changes
+# conn.commit()
 
 
 
-# close the cursor and the database connection
-cur.close()
-conn.close()
+# # close the cursor and the database connection
+# cur.close()
+# conn.close()
